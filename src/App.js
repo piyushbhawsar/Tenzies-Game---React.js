01bf47f738efe935dfa90e111ab0d3e716d459f4
piyushbhawsar <111758968+piyushbhawsar@users.noreply.github.com>
2023-03-24 01:42:06 +0530

    initial commits

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <h1>{`Jeda Nashaa ...Nasha ..
+        teri ankha vich jo aawe ...
+        mai dekheya bada`}
+      </h1>
+    </div>
+  );
+}
+
+export default App;
